a541fd24941824eb0d6f86108ab4c1822dfbd1cd
kpecord <73260789+kpecord@users.noreply.github.com>
2020-10-21 18:45:24 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+$(document).ready(function(){
+	$('.mobile-view').click(function(){
+		$('.desktop-view').toggleClass('expand');
+		
+	});
+});
+
+$(document).ready(function(){
+	$('.services').click(function(){
+		$('.service-view').toggleClass('expand');
+	});
+});
